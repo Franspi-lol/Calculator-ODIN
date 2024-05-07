ddf98caaacc4d36bef04d7fb913b0f2e6de81009
Franspi-lol <francobernar@gmail.com>
2024-05-06 21:12:55 -0300

    function complete+basic css(layout)

--- scripts.js
+++ scripts.js
@@ -11,10 +11,13 @@ function divide(n1, n2) {
   return n1 / n2;
 }
 
-var number1= 0;
-var number2= 0;
-var operator='';
+var number1 = 0;
+var number2 = 0;
+var storedValue = false;
+var resolved = true;
+var operator = "";
 var displayValue = document.getElementById("displayValue");
+var decimal = false;
 
 function operate(n1, n2, operator) {
   switch (operator) {
@@ -34,37 +37,84 @@ function operate(n1, n2, operator) {
 var arrBoton = [...document.querySelectorAll(".number")];
 arrBoton.forEach((boton) => {
   boton.addEventListener("click", () => {
-    changeDisplay(boton.id)
+    changeDisplay(boton.id);
   });
 });
 
-document.getElementById('clr').addEventListener('click', ()=>{
-    displayValue.innerText = '';
+document.getElementById("clr").addEventListener("click", () => {
+  clearAll();
+});
+
+document.getElementById(".").addEventListener("click", ()=>{
+  if (displayValue.innerText.indexOf(".")==-1) {
+    changeDisplay(".");
+  }
+  
 })
 
 function changeDisplay(number) {
-  displayValue.innerText = displayValue.innerText + number;
+  if (storedValue == false) {
+    displayValue.innerText = displayValue.innerText + number;
+  } else {
+    number1 = 0;
+    number2 = 0;
+    operator = 0;
+    storedValue = false;
+    displayValue.innerText = number;
+  }
 }
-console.log(displayValue.innerText);
 
-var arrOperador = [...document.querySelectorAll('.operator')];
-arrOperador.forEach((boton)=>{
-    boton.addEventListener('click',()=>{
-        /* if(number1==0) */
-        {
-            number1=parseInt(displayValue.textContent);
-            displayValue.innerText = '';
-            operator=boton.id;
-        }
-    })
-})
 
-document.getElementById('=').addEventListener('click', ()=>{
-    /* if(operator!='') */
+var arrOperador = [...document.querySelectorAll(".operator")];
+arrOperador.forEach((boton) => {
+  boton.addEventListener("click", () => {
     {
-        number2=parseInt(displayValue.textContent);
-        displayValue.innerText=operate(number1, number2, operator);
-        number1=parseInt(displayValue.innerText);
-        console.log(number1);
+      if (resolved == true) {
+        number1 = parseFloat(displayValue.textContent);
+      } else {
+        number2 = parseFloat(displayValue.textContent);
+        if (!checkDiv0(operator, number2)) {
+          number1 = operate(number1, number2, operator);
+        }
+      }
+      displayValue.innerText = "";
+      operator = boton.id;
+      storedValue = false;
+      resolved = false;
     }
-})
+  });
+});
+
+document.getElementById("=").addEventListener("click", () => {
+  if (storedValue == false) {
+    number2 = parseFloat(displayValue.textContent);
+    if (!checkDiv0(operator, number2)) {
+      displayValue.innerText = operate(number1, number2, operator);
+      number1 = parseFloat(displayValue.innerText);
+      storedValue = true;
+    }
+  } else {
+    displayValue.innerText = operate(number1, number2, operator);
+    number1 = parseFloat(displayValue.innerText);
+  }
+  resolved = true;
+});
+
+function clearAll() {
+  displayValue.innerText = "";
+  number1 = 0;
+  number2 = 0;
+  operator = 0;
+  storedValue = false;
+  resolved = true;
+}
+
+function checkDiv0(operator, num2) {
+  if (operator == "/" && num2 == 0) {
+    alert("No dividas por cero");
+    clearAll();
+    return true;
+  } else {
+    return false;
+  }
+}
